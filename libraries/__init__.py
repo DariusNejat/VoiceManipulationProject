21c521111d1c5b23c077d37d55f75ff984987445
HaniyeMahmoodi <169162364+HaniyeMahmoodi@users.noreply.github.com>
2025-02-05 23:52:22 +0330

    Update __init__.py

--- libraries/__init__.py
+++ libraries/__init__.py
@@ -4,14 +4,14 @@
 from .library1 import segment_voice  # Example function from library1.py
 from .library2 import reverse_segments  # Example function from library2.py
 from .library3 import change_pitch  # Example function from library3.py
-from .library4 import adjust_speed  # Example function from library4.py
+from .library4 import segment_audio_list  # Example function from library4.py
 from .library5 import smooth_audio_list  # Example function from library5.py
 from .library6 import tone_down  # Example function from library6.py
 # Add more imports as needed for all libraries
 
 # List of all imported functions (for easier access in main.py)
 __all__ = [
-    "segment_voice",
+    "segment_audio_list",
     "reverse_segments",
     "smooth_audio_list",
     "adjust_speed",
